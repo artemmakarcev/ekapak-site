075bf7172e037c28e7a524eb33c212b77e7ecd22
Artem <makarcev4@gmail.com>
2025-10-10 14:02:55 +0500

    added api
    Добавил необходимые api запросы

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,33 @@
 import axios from "axios";
-const API_KEY = import.meta.env.VITE_API_KEY;
 const API_URL = import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
   baseURL: API_URL,
-  withCredentials: true,
-  headers: { "API-KEY": API_KEY },
+  withCredentials: false,
 });
 
-export instance;
+export const productAPI = {
+  async getAllProducts(page: number) {
+    const response = await instance.get(`products?page=${page}`);
+    return response.data;
+  },
+  async getAllProDuctsByCategory(uuid: string, page: number) {
+    const response = await instance.get(`products?category=${uuid}&page=${page}`);
+    return response.data;
+  },
+  async getProductByUUID(uuid: string) {
+    const response = await instance.get(`products/${uuid}`);
+    return response.data;
+  },
+  async getProductBySlug(slug: string) {
+    const response = await instance.get(`products/slug/${slug}`);
+    return response.data;
+  },
+};
+
+export const categoryAPI = {
+  async getAllCategory() {
+    const response = await instance.get(`categories`);
+    return response.data;
+  },
+};
